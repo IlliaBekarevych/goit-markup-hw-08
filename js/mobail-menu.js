2ee67983f0e3fac791c3f49e2df03f3c4f8353c1
IlliaBekarevych <illiabekarevych@gmail.com>
2022-01-06 18:46:22 +0200

    Выполняет полностью дз8

---
+++ js/mobail-menu.js
@@ -0,0 +1,14 @@
+    (() => {
+    const refs = {
+        openModalBtn: document.querySelector('[data-mobail-open]'),
+        closeModalBtn: document.querySelector('[data-mobail-close]'),
+        modal: document.querySelector('[data-mobail]'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle('is-hidden');
+    }
+    })();
